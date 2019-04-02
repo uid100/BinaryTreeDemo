7e7e22f050adb36ed14fb57c8c54187c25c7a3ec
dev1 <dev1@officewires.com>
2019-04-02 10:54:45 -0700

    Add project files.

---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public class Tree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Tree<T> LeftTree { get; set; }
+        public Tree<T> RightTree { get; set; }
+
+        public Tree(T nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        public void Insert(T newItem)
+        {
+            if(this.NodeData.CompareTo(newItem) > 0)
+            {
+                // left tree
+                if(this.LeftTree == null)
+                {
+                    this.LeftTree = new Tree<T>(newItem);
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                // right tree
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new Tree<T>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+
+        public string WalkTree()
+        {
+            string result = "";
+
+            if(this.LeftTree!=null)
+            {
+                result = this.LeftTree.WalkTree();
+            }
+            result += $" {this.NodeData.ToString()} ";
+            if(this.RightTree!=null)
+            {
+                result += this.RightTree.WalkTree();
+            }
+            return result;
+        }
+    }
+}
---
+++ BinaryTreeTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using BinaryTree;
+
+namespace BinaryTreeTest
+{
+    class Program
+    {
+        static void binarySortInt()
+        {
+            Tree<int> tree1 = new Tree<int>(10);
+            tree1.Insert(5);
+            tree1.Insert(11);
+            tree1.Insert(5);
+            tree1.Insert(-12);
+            tree1.Insert(15);
+            tree1.Insert(0);
+            tree1.Insert(14);
+            tree1.Insert(-8);
+            tree1.Insert(10);
+            tree1.Insert(8);
+            tree1.Insert(8);
+
+            Console.WriteLine(tree1.WalkTree());
+        }
+
+        static void binarySortStrings()
+        {
+            Tree<string> tree2 = new Tree<string>("Hello");
+            tree2.Insert("World");
+            tree2.Insert("How");
+            tree2.Insert("Are");
+            tree2.Insert("You");
+            tree2.Insert("Today");
+            tree2.Insert("I");
+            tree2.Insert("Hope");
+            tree2.Insert("You");
+            tree2.Insert("Are");
+            tree2.Insert("Feeling");
+            tree2.Insert("Well");
+            tree2.Insert("!");
+
+            Console.WriteLine(tree2.WalkTree());
+        }
+
+        static void binarySortStudents()
+        {
+            Tree<string> tree3 = new Tree<string>("Garrett");
+            tree3.Insert("Darwin");
+            tree3.Insert("Tyler");
+            tree3.Insert("Bryan");
+            tree3.Insert("Mako");
+            tree3.Insert("David");
+            tree3.Insert("Cody");
+            tree3.Insert("Dennis");
+            tree3.Insert("Alex");
+            tree3.Insert("Aaron");
+
+            Console.WriteLine(tree3.WalkTree());
+        }
+
+        static void Main(string[] args)
+        {
+            binarySortStudents();
+            Console.WriteLine();
+            binarySortStrings();
+        }
+    }
+}
